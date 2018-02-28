6b3200cc16e8097fd8a65df307937fbd12fa8d66
izumin5210 <m@izum.in>
2018-02-28 23:55:11 +0900

    make gen

--- pkg/grapicmd/generate/template/command.go
+++ pkg/grapicmd/generate/template/command.go
@@ -6,10 +6,10 @@ import (
 	"github.com/jessevdk/go-assets"
 )
 
-var _Command9acbc40e153d42b0b3e06ca83e7c53e3670d8cdc = "package main\n\n// Run starts awesome process.\nfunc Run(args []string) error {\n\treturn nil\n}\n"
+var _Command9acbc40e153d42b0b3e06ca83e7c53e3670d8cdc = "package main\n\nimport (\n\t\"os\"\n)\n\nfunc main() int {\n\tos.Exit(run())\n}\n\nfunc run() int {\n\treturn 0\n}\n"
 
 // Command returns go-assets FileSystem
-var Command = assets.NewFileSystem(map[string][]string{"/cmd/{{ .name }}": []string{"run.go.tmpl"}, "/": []string{}, "/cmd": []string{}}, map[string]*assets.File{
+var Command = assets.NewFileSystem(map[string][]string{"/": []string{}, "/cmd": []string{}, "/cmd/{{ .name }}": []string{"run.go.tmpl"}}, map[string]*assets.File{
 	"/": &assets.File{
 		Path:     "/",
 		FileMode: 0x800001ed,
@@ -23,11 +23,11 @@ var Command = assets.NewFileSystem(map[string][]string{"/cmd/{{ .name }}": []str
 	}, "/cmd/{{ .name }}": &assets.File{
 		Path:     "/cmd/{{ .name }}",
 		FileMode: 0x800001ed,
-		Mtime:    time.Unix(1519539347, 1519539347000000000),
+		Mtime:    time.Unix(1519829745, 1519829745000000000),
 		Data:     nil,
 	}, "/cmd/{{ .name }}/run.go.tmpl": &assets.File{
 		Path:     "/cmd/{{ .name }}/run.go.tmpl",
 		FileMode: 0x1a4,
-		Mtime:    time.Unix(1519539045, 1519539045000000000),
+		Mtime:    time.Unix(1519829745, 1519829745000000000),
 		Data:     []byte(_Command9acbc40e153d42b0b3e06ca83e7c53e3670d8cdc),
 	}}, "")
