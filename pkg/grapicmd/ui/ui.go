5ed7d34898f0b6acaf6bc324d938ed559a6be038
izumin5210 <m@izum.in>
2018-03-04 01:17:30 +0900

    Improve UI

--- pkg/grapicmd/ui/ui.go
+++ pkg/grapicmd/ui/ui.go
@@ -3,23 +3,113 @@ package ui
 import (
 	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/fatih/color"
 )
 
-// Status represents is a level and a message prefix.
-type Status interface {
-	fmt.Stringer
-	Level() Level
+type fprintFunc func(w io.Writer, msg string)
+
+type printConfig struct {
+	prefix     string
+	colorAttrs []color.Attribute
+	indent     int
+	allColor   bool
+}
+
+type printType int
+
+const (
+	printTypeOutput printType = iota
+	printTypeSection
+	printTypeSubsection
+	printTypeWarn
+	printTypeError
+	printTypeItemSuccess
+	printTypeItemSkipped
+	printTypeItemFailure
+)
+
+const (
+	indentSizeItem = 4
+)
+
+var (
+	configByPrintType = map[printType]printConfig{
+		printTypeSection: printConfig{
+			prefix:     "➜",
+			colorAttrs: []color.Attribute{color.FgYellow},
+			allColor:   true,
+		},
+		printTypeSubsection: printConfig{
+			prefix:     "▸",
+			colorAttrs: []color.Attribute{color.FgBlue},
+			allColor:   true,
+		},
+		printTypeWarn: printConfig{
+			prefix:     "⚠",
+			colorAttrs: []color.Attribute{color.FgHiYellow},
+			allColor:   true,
+		},
+		printTypeError: printConfig{
+			prefix:     "☓",
+			colorAttrs: []color.Attribute{color.FgHiRed},
+			allColor:   true,
+		},
+		printTypeItemSuccess: printConfig{
+			prefix:     "✔",
+			colorAttrs: []color.Attribute{color.Bold, color.FgGreen},
+			indent:     indentSizeItem,
+		},
+		printTypeItemSkipped: printConfig{
+			prefix:     "╌",
+			colorAttrs: []color.Attribute{color.Bold, color.FgBlue},
+			indent:     indentSizeItem,
+		},
+		printTypeItemFailure: printConfig{
+			prefix:     "✗",
+			colorAttrs: []color.Attribute{color.Bold, color.FgRed},
+			indent:     indentSizeItem,
+		},
+	}
+	fprintlnFuncByPrintType = map[printType]fprintFunc{}
+)
+
+func init() {
+	for pt, cfg := range configByPrintType {
+		cfg := cfg
+		fmtStr := "%s"
+		if cfg.indent > 0 {
+			fmtStr = "%" + strconv.FormatInt(int64(cfg.indent), 10) + "s"
+		}
+		if cfg.allColor {
+			colored := color.New(cfg.colorAttrs...).FprintfFunc()
+			fprintlnFuncByPrintType[pt] = func(w io.Writer, msg string) {
+				colored(w, "  "+fmtStr+"  %s\n", cfg.prefix, msg)
+			}
+		} else {
+			prefix := color.New(cfg.colorAttrs...).Sprintf(fmtStr, cfg.prefix)
+			fprintlnFuncByPrintType[pt] = func(w io.Writer, msg string) {
+				fmt.Fprintf(w, "  %s  %s\n", prefix, msg)
+			}
+		}
+	}
+}
+
+func (pt printType) Fprintln(w io.Writer, msg string) {
+	fprintlnFuncByPrintType[pt](w, msg)
 }
 
 // UI is an interface for intaracting with the terminal.
 type UI interface {
-	PrintWithStatus(msg string, status Status)
-	PrintSuccess(msg, status string)
-	PrintInfo(msg, status string)
-	PrintWarn(msg, status string)
-	PrintFail(msg, status string)
+	Output(msg string)
+	Section(msg string)
+	Subsection(msg string)
+	Warn(msg string)
+	Error(msg string)
+	ItemSuccess(msg string)
+	ItemSkipped(msg string)
+	ItemFailure(msg string)
 }
 
 // New creates a new UI instance.
@@ -33,27 +123,34 @@ type uiImpl struct {
 	out io.Writer
 }
 
-func (u *uiImpl) PrintWithStatus(msg string, st Status) {
-	u.printWithLevel(msg, st.String(), st.Level())
+func (u *uiImpl) Output(msg string) {
+	fmt.Fprintf(u.out, "    %s\n", msg)
 }
 
-func (u *uiImpl) PrintSuccess(msg, status string) {
-	u.printWithLevel(msg, status, LevelSuccess)
+func (u *uiImpl) Section(msg string) {
+	printTypeSection.Fprintln(u.out, msg)
 }
 
-func (u *uiImpl) PrintInfo(msg, status string) {
-	u.printWithLevel(msg, status, LevelInfo)
+func (u *uiImpl) Subsection(msg string) {
+	printTypeSubsection.Fprintln(u.out, msg)
 }
 
-func (u *uiImpl) PrintWarn(msg, status string) {
-	u.printWithLevel(msg, status, LevelWarn)
+func (u *uiImpl) Warn(msg string) {
+	printTypeWarn.Fprintln(u.out, msg)
 }
 
-func (u *uiImpl) PrintFail(msg, status string) {
-	u.printWithLevel(msg, status, LevelFail)
+func (u *uiImpl) Error(msg string) {
+	printTypeError.Fprintln(u.out, msg)
 }
 
-func (u *uiImpl) printWithLevel(msg, status string, lv Level) {
-	colored := color.New(lv.colorAttrs()...).SprintfFunc()
-	fmt.Fprintf(u.out, "%s  %s\n", colored("%12s", status), msg)
+func (u *uiImpl) ItemSuccess(msg string) {
+	printTypeItemSuccess.Fprintln(u.out, msg)
+}
+
+func (u *uiImpl) ItemSkipped(msg string) {
+	printTypeItemSkipped.Fprintln(u.out, msg)
+}
+
+func (u *uiImpl) ItemFailure(msg string) {
+	printTypeItemFailure.Fprintln(u.out, msg)
 }
