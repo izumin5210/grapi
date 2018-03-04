9d1943ddc3df7ed384cc72c93f30fbcedd093618
izumin5210 <m@izum.in>
2018-03-04 15:52:12 +0900

    Use new command executor impl

--- pkg/grapicmd/cmd/user_defined.go
+++ pkg/grapicmd/cmd/user_defined.go
@@ -2,13 +2,13 @@ package cmd
 
 import (
 	"fmt"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	"github.com/izumin5210/grapi/pkg/grapicmd"
+	"github.com/izumin5210/grapi/pkg/grapicmd/command"
 	"github.com/izumin5210/grapi/pkg/grapicmd/util/fs"
 )
 
@@ -21,22 +21,21 @@ func newUserDefinedCommand(cfg grapicmd.Config, rootDir, entryPath string) *cobr
 		SilenceErrors: true,
 		SilenceUsage:  true,
 		RunE: func(c *cobra.Command, args []string) error {
+			executor := command.NewExecutor(rootDir, cfg.OutWriter(), cfg.ErrWriter(), cfg.InReader())
+
 			err := fs.CreateDirIfNotExists(cfg.Fs(), binDir)
 			if err != nil {
 				return errors.WithStack(err)
 			}
 
-			out, err := exec.Command("go", "build", "-v", "-o="+binPath, entryPath).CombinedOutput()
+			out, err := executor.Exec([]string{"go", "build", "-v", "-o=" + binPath, entryPath})
 			if err != nil {
 				fmt.Println(string(out))
 				return errors.Wrapf(err, "failed to build %q", entryPath)
 			}
 
-			cmd := exec.Command(binPath)
-			cmd.Stdin = cfg.InReader()
-			cmd.Stdout = cfg.OutWriter()
-			cmd.Stderr = cfg.ErrWriter()
-			return errors.WithStack(cmd.Run())
+			_, err = executor.Exec([]string{binPath}, command.WithIOConnected())
+			return errors.WithStack(err)
 		},
 	}
 }
