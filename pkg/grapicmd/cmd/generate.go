b083c052e667fec81a44adca7c6207adf6ff6c55
izumin5210 <m@izum.in>
2018-03-06 17:05:46 +0900

    Impl GenerateServiceUsecase

--- pkg/grapicmd/cmd/generate.go
+++ pkg/grapicmd/cmd/generate.go
@@ -1,18 +1,14 @@
 package cmd
 
 import (
-	"path/filepath"
-	"strings"
-
 	"github.com/pkg/errors"
-	"github.com/serenize/snaker"
 	"github.com/spf13/cobra"
 
 	"github.com/izumin5210/grapi/pkg/grapicmd"
 	"github.com/izumin5210/grapi/pkg/grapicmd/generate"
 	"github.com/izumin5210/grapi/pkg/grapicmd/generate/template"
+	"github.com/izumin5210/grapi/pkg/grapicmd/internal/usecase"
 	"github.com/izumin5210/grapi/pkg/grapicmd/ui"
-	"github.com/izumin5210/grapi/pkg/grapicmd/util/fs"
 )
 
 func newGenerateCommand(cfg grapicmd.Config, ui ui.UI) *cobra.Command {
@@ -39,58 +35,9 @@ func newGenerateServiceCommand(cfg grapicmd.Config, ui ui.UI) *cobra.Command {
 				return errors.New("geneate command should execut inside a grapi applicaiton directory")
 			}
 
-			// github.com/foo/bar
-			importPath, err := fs.GetImportPath(cfg.RootDir())
-			if err != nil {
-				return errors.WithStack(err)
-			}
-			// baz/qux/quux
-			path := args[0]
-
-			// quux
-			name := filepath.Base(path)
-			// Quux
-			serviceName := snaker.SnakeToCamel(name)
-
-			// baz/qux
-			packagePath := filepath.Dir(path)
-			// qux
-			packageName := filepath.Base(packagePath)
-
-			// api/baz/qux
-			pbgoPackagePath := filepath.Join("api", packagePath)
-			// qux_pb
-			pbgoPackageName := filepath.Base(pbgoPackagePath) + "_pb"
-
-			if packagePath == "." {
-				packagePath = "server"
-				packageName = packagePath
-				pbgoPackagePath = "api_pb"
-				pbgoPackageName = pbgoPackagePath
-			}
-
-			protoPackageChunks := []string{}
-			for _, pkg := range strings.Split(filepath.Join(importPath, "api", filepath.Dir(path)), "/") {
-				chunks := strings.Split(pkg, ".")
-				for i := len(chunks) - 1; i >= 0; i-- {
-					protoPackageChunks = append(protoPackageChunks, chunks[i])
-				}
-			}
-			// com.github.foo.bar.baz.qux
-			protoPackage := strings.Join(protoPackageChunks, ".")
-
-			data := map[string]interface{}{
-				"importPath":      importPath,
-				"path":            path,
-				"name":            name,
-				"serviceName":     serviceName,
-				"packagePath":     packagePath,
-				"packageName":     packageName,
-				"pbgoPackagePath": pbgoPackagePath,
-				"pbgoPackageName": pbgoPackageName,
-				"protoPackage":    protoPackage,
-			}
-			return generate.NewGenerator(cfg.Fs(), ui, cfg.RootDir()).Run(template.Service, data)
+			generator := generate.NewGenerator(cfg.Fs(), ui, cfg.RootDir())
+			generateUsecase := usecase.NewGenerateServiceUsecase(ui, generator, cfg.RootDir())
+			return errors.WithStack(generateUsecase.Perform(args[0]))
 		},
 	}
 }
