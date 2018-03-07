479176ffa44fb7ea82ad42573cb022a4f49dcaac
izumin5210 <m@izum.in>
2018-03-07 14:38:00 +0900

    Fix test

--- pkg/grapicmd/internal/script_test.go
+++ pkg/grapicmd/internal/script_test.go
@@ -5,33 +5,46 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/izumin5210/grapi/pkg/grapicmd/command"
+	"github.com/izumin5210/grapi/pkg/grapicmd/internal/module"
 	"github.com/spf13/afero"
 )
 
 func Test_Script(t *testing.T) {
+	type execution struct {
+		nameAndArgs []string
+		dir         string
+		connected   bool
+	}
 	type testContext struct {
 		factory    ScriptFactory
 		fs         afero.Fs
 		rootDir    string
-		executions [][]string
+		executions []*execution
 	}
 
 	createTestContext := func(t *testing.T) *testContext {
 		ctx := &testContext{
 			fs:         afero.NewMemMapFs(),
 			rootDir:    "/home/app",
-			executions: [][]string{},
+			executions: []*execution{},
 		}
 
-		executor := &fakeExecutor{
-			fakeExec: func(nameAndArgs []string, _ ...command.Option) ([]byte, error) {
-				ctx.executions = append(ctx.executions, nameAndArgs)
-				return []byte{}, nil
+		commandFactory := &fakeCommandFactory{
+			fakeCreate: func(nameAndArgs []string) module.Command {
+				c := &fakeCommand{}
+				c.fakeExec = func() ([]byte, error) {
+					ctx.executions = append(ctx.executions, &execution{
+						nameAndArgs: nameAndArgs,
+						dir:         c.dir,
+						connected:   c.ioConnected,
+					})
+					return []byte{}, nil
+				}
+				return c
 			},
 		}
 
-		ctx.factory = NewScriptFactory(ctx.fs, executor, ctx.rootDir)
+		ctx.factory = NewScriptFactory(ctx.fs, commandFactory, ctx.rootDir)
 
 		return ctx
 	}
@@ -52,8 +65,14 @@ func Test_Script(t *testing.T) {
 		t.Errorf("Build() returned an error %v", err)
 	}
 
-	if got, want := testCtx.executions[0], []string{"go", "build", "-v", "-o=" + bin, entry}; !reflect.DeepEqual(got, want) {
+	exec := testCtx.executions[0]
+
+	if got, want := exec.nameAndArgs, []string{"go", "build", "-v", "-o=" + bin, entry}; !reflect.DeepEqual(got, want) {
 		t.Errorf("Build() executed %v, want %v", got, want)
+	}
+
+	if got, want := exec.dir, "/home/app"; got != want {
+		t.Errorf("Build() executed a command in %v, want %v", got, want)
 	}
 
 	err = s.Run()
@@ -61,18 +80,45 @@ func Test_Script(t *testing.T) {
 		t.Errorf("Run() returned an error %v", err)
 	}
 
-	if got, want := testCtx.executions[1], []string{bin}; !reflect.DeepEqual(got, want) {
+	exec = testCtx.executions[1]
+
+	if got, want := exec.nameAndArgs, []string{bin}; !reflect.DeepEqual(got, want) {
 		t.Errorf("Run() executed %v, want %v", got, want)
+	}
+
+	if got, want := exec.dir, "/home/app"; got != want {
+		t.Errorf("Run() executed a command in %v, want %v", got, want)
 	}
 }
 
 // fake impls
 
-type fakeExecutor struct {
-	command.Executor
-	fakeExec func([]string, ...command.Option) ([]byte, error)
+type fakeCommandFactory struct {
+	module.CommandFactory
+	fakeCreate func([]string) module.Command
 }
 
-func (e *fakeExecutor) Exec(nameAndArgs []string, opts ...command.Option) ([]byte, error) {
-	return e.fakeExec(nameAndArgs, opts...)
+func (f *fakeCommandFactory) Create(nameAndArgs []string) module.Command {
+	return f.fakeCreate(nameAndArgs)
+}
+
+type fakeCommand struct {
+	module.Command
+	dir         string
+	ioConnected bool
+	fakeExec    func() ([]byte, error)
+}
+
+func (c *fakeCommand) SetDir(dir string) module.Command {
+	c.dir = dir
+	return c
+}
+
+func (c *fakeCommand) ConnectIO() module.Command {
+	c.ioConnected = true
+	return c
+}
+
+func (c *fakeCommand) Exec() ([]byte, error) {
+	return c.fakeExec()
 }
