1eb109574e1af550ba0031b774fdbc72b73482d0
izumin5210 <m@izum.in>
2018-03-17 14:10:42 +0900

    Generate Script and ScriptLoader mock

--- pkg/grapicmd/internal/module/gen.go
+++ pkg/grapicmd/internal/module/gen.go
@@ -1,4 +1,5 @@
 //go:generate mockgen -package=moduletesting -source=ui.go -destination=testing/ui_mock.go
 //go:generate mockgen -package=moduletesting -imports=.=github.com/izumin5210/grapi/pkg/grapicmd/internal/module -source=generator.go -destination=testing/generator_mock.go
+//go:generate mockgen -package=moduletesting -imports=.=github.com/izumin5210/grapi/pkg/grapicmd/internal/module -source=script.go -destination=testing/script_mock.go
 
 package module
