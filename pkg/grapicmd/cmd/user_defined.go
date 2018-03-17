05ced5a4d25720f19b67d64a97f1de890dbd2cd0
izumin5210 <m@izum.in>
2018-03-17 12:46:10 +0900

    Use ScriptLoader

--- pkg/grapicmd/cmd/user_defined.go
+++ pkg/grapicmd/cmd/user_defined.go
@@ -7,12 +7,17 @@ import (
 	"github.com/izumin5210/grapi/pkg/grapicmd/internal/module"
 )
 
-func newUserDefinedCommand(ui module.UI, script module.Script) *cobra.Command {
+func newUserDefinedCommand(ui module.UI, scriptLoader module.ScriptLoader, name string) *cobra.Command {
 	return &cobra.Command{
-		Use:           script.Name(),
+		Use:           name,
 		SilenceErrors: true,
 		SilenceUsage:  true,
 		RunE: func(c *cobra.Command, args []string) (err error) {
+			script, ok := scriptLoader.Get(name)
+			if !ok {
+				err = errors.Wrapf(err, "faild to find subcommand %d", name)
+			}
+
 			ui.Section(script.Name())
 			ui.Subsection("Building...")
 			err = errors.WithStack(script.Build())
