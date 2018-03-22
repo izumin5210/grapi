d6d1ff0e2cef79865b5c74787922b39ba179c362
izumin5210 <m@izum.in>
2018-03-22 20:30:56 +0900

    Impl generator constructor func

--- pkg/grapicmd/internal/module/generator/generator.go
+++ pkg/grapicmd/internal/module/generator/generator.go
@@ -1,12 +1,18 @@
 package generator
 
 import (
+	"github.com/spf13/afero"
+
 	"github.com/izumin5210/grapi/pkg/grapicmd/internal/module"
 )
 
 // New creates a module.Generator instance.
-func New() module.Generator {
-	return &generator{}
+func New(fs afero.Fs, ui module.UI, rootDir, version string) module.Generator {
+	return &generator{
+		ProjectGenerator: newProjectGenerator(fs, ui, version),
+		ServiceGenerator: newServiceGenerator(fs, ui, rootDir),
+		CommandGenerator: newCommandGenerator(fs, ui, rootDir),
+	}
 }
 
 type generator struct {
