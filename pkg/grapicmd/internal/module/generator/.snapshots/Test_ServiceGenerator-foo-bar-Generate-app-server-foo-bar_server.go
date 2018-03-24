7b7f48df1b1e42d2a03311f3b86052ea42bdeeec
izumin5210 <m@izum.in>
2018-03-24 21:54:03 +0900

    Add test cases for ServiceGenerator

---
+++ pkg/grapicmd/internal/module/generator/.snapshots/Test_ServiceGenerator-foo-bar-Generate-app-server-foo-bar_server.go
@@ -0,0 +1,37 @@
+package foo
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	foo_pb "testapp/api/foo"
+)
+
+var (
+	// RegisterBarServiceHandler is a function to register card service handler to gRPC Gateway's mux.
+	RegisterBarServiceHandler = foo_pb.RegisterBarServiceHandler
+)
+
+// RegisterBarServiceServerFactory creates a function to register card service server impl to grpc.Server.
+func RegisterBarServiceServerFactory() func(s *grpc.Server) {
+	return func(s *grpc.Server) {
+		foo_pb.RegisterBarServiceServer(s, New())
+	}
+}
+
+// New creates a new BarServiceServer instance.
+func New() foo_pb.BarServiceServer {
+	return &barServiceServerImpl{}
+}
+
+type barServiceServerImpl struct {
+}
+
+func (s *barServiceServerImpl) GetBar(ctx context.Context, req *foo_pb.GetBarRequest) (*foo_pb.GetBarResponse, error) {
+	// TODO: Not yet implemented.
+	return nil, status.Error(codes.Unimplemented, "TODO: You should implement it!")
+}
+
--- pkg/grapicmd/internal/module/generator/service_test.go
+++ pkg/grapicmd/internal/module/generator/service_test.go
@@ -31,51 +31,83 @@ func Test_ServiceGenerator(t *testing.T) {
 
 	generator := newServiceGenerator(fs, ui, rootDir)
 
-	name := "foo/bar-baz"
-	files := []string{
-		"api/protos/foo/bar_baz.proto",
-		"app/server/foo/bar_baz_server.go",
+	cases := []struct {
+		name  string
+		files []string
+	}{
+		{
+			name: "foo",
+			files: []string{
+				"api/protos/foo.proto",
+				"app/server/foo_server.go",
+			},
+		},
+		{
+			name: "foo/bar",
+			files: []string{
+				"api/protos/foo/bar.proto",
+				"app/server/foo/bar_server.go",
+			},
+		},
+		{
+			name: "foo/bar_baz",
+			files: []string{
+				"api/protos/foo/bar_baz.proto",
+				"app/server/foo/bar_baz_server.go",
+			},
+		},
+		{
+			name: "foo/bar-baz",
+			files: []string{
+				"api/protos/foo/bar_baz.proto",
+				"app/server/foo/bar_baz_server.go",
+			},
+		},
 	}
 
-	t.Run("Generate", func(t *testing.T) {
-		err := generator.GenerateService(name)
-
-		if err != nil {
-			t.Errorf("returned an error %v", err)
-		}
-
-		for _, file := range files {
-			t.Run(file, func(t *testing.T) {
-				data, err := afero.ReadFile(fs, filepath.Join(rootDir, file))
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			t.Run("Generate", func(t *testing.T) {
+				err := generator.GenerateService(c.name)
 
 				if err != nil {
 					t.Errorf("returned an error %v", err)
 				}
 
-				cupaloy.SnapshotT(t, string(data))
+				for _, file := range c.files {
+					t.Run(file, func(t *testing.T) {
+						data, err := afero.ReadFile(fs, filepath.Join(rootDir, file))
+
+						if err != nil {
+							t.Errorf("returned an error %v", err)
+						}
+
+						cupaloy.SnapshotT(t, string(data))
+					})
+				}
 			})
-		}
-	})
 
-	t.Run("Destroy", func(t *testing.T) {
-		err := generator.DestroyService(name)
-
-		if err != nil {
-			t.Errorf("returned an error %v", err)
-		}
-
-		for _, file := range files {
-			t.Run(file, func(t *testing.T) {
-				ok, err := afero.Exists(fs, filepath.Join(rootDir, file))
+			t.Run("Destroy", func(t *testing.T) {
+				err := generator.DestroyService(c.name)
 
 				if err != nil {
-					t.Errorf("Exists(fs, %q) returned an error %v", file, err)
+					t.Errorf("returned an error %v", err)
 				}
 
-				if ok {
-					t.Errorf("%q should not exist", file)
+				for _, file := range c.files {
+					t.Run(file, func(t *testing.T) {
+						ok, err := afero.Exists(fs, filepath.Join(rootDir, file))
+
+						if err != nil {
+							t.Errorf("Exists(fs, %q) returned an error %v", file, err)
+						}
+
+						if ok {
+							t.Errorf("%q should not exist", file)
+						}
+					})
 				}
 			})
-		}
-	})
+		})
+	}
 }
