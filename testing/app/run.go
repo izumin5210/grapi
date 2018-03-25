ea6c97705979c3bf84fc6554edf2449f4308ce88
izumin5210 <m@izum.in>
2018-03-25 18:55:00 +0900

    grapi init --HEAD testing

---
+++ testing/app/run.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"github.com/izumin5210/grapi/pkg/grapiserver"
+)
+
+// Run starts the grapiserver.
+func Run() error {
+	return grapiserver.New().
+		UseDefaultLogger().
+		AddServers(
+		// TODO
+		).
+		Serve()
+}
