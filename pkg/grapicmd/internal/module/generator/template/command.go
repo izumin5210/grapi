0fbfff2d5a915ea1c15b7435a943615fdf2479b5
izumin5210 <m@izum.in>
2018-04-13 21:37:43 +0900

    make gen

--- pkg/grapicmd/internal/module/generator/template/command.go
+++ pkg/grapicmd/internal/module/generator/template/command.go
@@ -9,7 +9,7 @@ import (
 var _Command9acbc40e153d42b0b3e06ca83e7c53e3670d8cdc = "package main\n\nimport (\n\t\"fmt\"\n\t\"os\"\n)\n\nfunc main() {\n\tos.Exit(run())\n}\n\nfunc run() int {\n\tfmt.Println(\"It works!\")\n\treturn 0\n}\n"
 
 // Command returns go-assets FileSystem
-var Command = assets.NewFileSystem(map[string][]string{"/": []string{}, "/cmd": []string{}, "/cmd/{{ .name }}": []string{"run.go.tmpl"}}, map[string]*assets.File{
+var Command = assets.NewFileSystem(map[string][]string{"/cmd/{{ .name }}": []string{"run.go.tmpl"}, "/": []string{}, "/cmd": []string{}}, map[string]*assets.File{
 	"/cmd/{{ .name }}": &assets.File{
 		Path:     "/cmd/{{ .name }}",
 		FileMode: 0x800001ed,
