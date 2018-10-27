0353cccf884fe4e1542ac0848c361a44f08303c2
izumin5210 <m@izum.in>
2018-10-27 20:22:20 +0900

    fixup! Move UI and IO impls into cli package

--- pkg/cli/types.go
+++ pkg/cli/types.go
@@ -2,13 +2,30 @@ package cli
 
 import (
 	"io"
+	"os"
 	"path/filepath"
+	"runtime"
+
+	colorable "github.com/mattn/go-colorable"
 )
 
 type IO struct {
 	In  io.Reader
 	Out io.Writer
 	Err io.Writer
+}
+
+func DefaultIO() *IO {
+	io := &IO{
+		In:  os.Stdin,
+		Out: os.Stdout,
+		Err: os.Stderr,
+	}
+	if runtime.GOOS == "windows" {
+		io.Out = colorable.NewColorableStdout()
+		io.Err = colorable.NewColorableStderr()
+	}
+	return io
 }
 
 type RootDir string
