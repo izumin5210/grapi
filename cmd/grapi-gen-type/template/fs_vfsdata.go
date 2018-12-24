538cc91d8e7843b9fc1e39142a3cf51373ed388d
izumin5210 <m@izum.in>
2018-12-24 11:34:51 +0900

    Fix type package

--- cmd/grapi-gen-type/template/fs_vfsdata.go
+++ cmd/grapi-gen-type/template/fs_vfsdata.go
@@ -21,23 +21,19 @@ var FS = func() http.FileSystem {
 	fs := vfsgen۰FS{
 		"/": &vfsgen۰DirInfo{
 			name:    "/",
-			modTime: time.Date(2018, 11, 11, 11, 23, 7, 0, time.UTC),
+			modTime: time.Date(2018, 11, 13, 1, 19, 2, 0, time.UTC),
 		},
 		"/api": &vfsgen۰DirInfo{
 			name:    "api",
-			modTime: time.Date(2018, 11, 11, 11, 23, 7, 0, time.UTC),
+			modTime: time.Date(2018, 11, 13, 1, 19, 2, 0, time.UTC),
 		},
 		"/api/protos": &vfsgen۰DirInfo{
 			name:    "protos",
-			modTime: time.Date(2018, 11, 12, 0, 26, 20, 0, time.UTC),
+			modTime: time.Date(2018, 12, 24, 2, 22, 36, 0, time.UTC),
 		},
-		"/api/protos/type": &vfsgen۰DirInfo{
-			name:    "type",
-			modTime: time.Date(2018, 11, 12, 0, 26, 20, 0, time.UTC),
-		},
-		"/api/protos/type/{{.Path}}.proto.tmpl": &vfsgen۰CompressedFileInfo{
+		"/api/protos/{{.Path}}.proto.tmpl": &vfsgen۰CompressedFileInfo{
 			name:             "{{.Path}}.proto.tmpl",
-			modTime:          time.Date(2018, 11, 11, 11, 29, 27, 0, time.UTC),
+			modTime:          time.Date(2018, 11, 13, 1, 19, 2, 0, time.UTC),
 			uncompressedSize: 163,
 
 			compressedContent: []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\x5c\xcd\x31\xca\x02\x31\x10\xc5\xf1\x7e\x4e\xf1\xc8\x01\x02\x1f\x5f\x19\x52\xdb\x49\x6e\x20\xa3\x2e\x31\xc8\x26\x61\x33\x85\x12\xe6\xee\x32\x8b\x5a\xd8\xfe\xf9\xf1\xde\x78\x56\xe1\x07\x22\x5c\xdf\x9a\xb4\x7f\x17\x88\x3a\x5f\xee\x9c\x17\xcc\x09\x9f\xac\xfa\xf4\x2e\xaa\x81\xa8\x75\x29\xad\x22\xb7\xd3\x07\x46\xb8\xdd\x9e\x0f\x5f\x9a\x58\x6e\xc6\x7f\xfb\x91\x57\x9b\xb1\x9b\x75\x19\x83\xf3\xfe\xe3\x2d\xab\x62\x12\x30\x64\x2b\x35\xa3\x5c\x11\xf1\x17\x48\xe9\x15\x00\x00\xff\xff\x17\x25\xa6\x2d\xa3\x00\x00\x00"),
@@ -50,10 +46,7 @@ var FS = func() http.FileSystem {
 		fs["/api/protos"].(os.FileInfo),
 	}
 	fs["/api/protos"].(*vfsgen۰DirInfo).entries = []os.FileInfo{
-		fs["/api/protos/type"].(os.FileInfo),
-	}
-	fs["/api/protos/type"].(*vfsgen۰DirInfo).entries = []os.FileInfo{
-		fs["/api/protos/type/{{.Path}}.proto.tmpl"].(os.FileInfo),
+		fs["/api/protos/{{.Path}}.proto.tmpl"].(os.FileInfo),
 	}
 
 	return fs
