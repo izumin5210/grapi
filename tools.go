c1a476327ae99c29c52c2af952649cd065161997
izumin5210 <m@izum.in>
2019-03-22 08:28:14 +0900

    Drop linters

--- tools.go
+++ tools.go
@@ -11,10 +11,6 @@ import (
 	_ "github.com/haya14busa/reviewdog/cmd/reviewdog"
 	_ "github.com/izumin5210/gex/cmd/gex"
 	_ "github.com/jessevdk/go-assets-builder"
-	_ "github.com/kisielk/errcheck"
 	_ "github.com/mitchellh/gox"
 	_ "github.com/srvc/wraperr/cmd/wraperr"
-	_ "golang.org/x/lint/golint"
-	_ "honnef.co/go/tools/cmd/megacheck"
-	_ "mvdan.cc/unparam"
 )
